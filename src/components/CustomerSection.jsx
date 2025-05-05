f405fb75696fd8952d9678658b75c98d653ad18f
Tom Carr <tom.carr@mytonautomotive.com>
2025-05-05 19:24:54 +0100

    Add dynamic customer loading functionality
    Introduced a stateful mechanism in `CustomerSection` to load and display customer data dynamically. Added example customer data in a new module to simulate the functionality. Inputs in the table are now read-only fields populated with selected customer details.

--- src/components/CustomerSection.jsx
+++ src/components/CustomerSection.jsx
@@ -1,17 +1,68 @@
 import styles from '../styles/CustomerSection.module.css';
+import customers from '../example_data/customers.js'
+import { useState } from 'react';
 
 function CustomerSection() {
+
+    const [customer, setCustomer] = useState({});
+
+    const loadCustomer = () => {
+        const customerToLoad = customers[4]; // later: let user select which one
+        setCustomer(customerToLoad);
+    };
+
+
     return (
         <div className={styles.section}>
-            <button className={styles.button}>LOAD CUSTOMER</button>
+            <button className={styles.button} onClick={loadCustomer}>LOAD CUSTOMER</button>
             <table className={styles.table}>
                 <tbody>
-                {["Customer Number", "Email", "FirstName", "LastName", "Address"].map((item, idx) => (
-                    <tr key={idx}>
-                        <td className={styles.label}>{item}</td>
-                        <td><input type="text" /></td>
-                    </tr>
-                ))}
+                <tr>
+                    <td className={styles.label}>Customer Number</td>
+                    <td><input type="text" value={customer.customerNumber || ''} readOnly /></td>
+                </tr>
+
+                <tr>
+                    <td className={styles.label}>Email Address</td>
+                    <td><input type="text" value={customer.emailAddress || ''} readOnly /></td>
+                </tr>
+
+                <tr>
+                    <td className={styles.label}>FirstName</td>
+                    <td><input type="text" value={customer.firstName || ''} readOnly /></td>
+                </tr>
+                <tr>
+                    <td className={styles.label}>LastName</td>
+                    <td><input type="text" value={customer.lastName || ''} readOnly /></td>
+                </tr>
+                <tr>
+                    <td className={styles.label}>Business</td>
+                    <td><input type="text" value={customer.business || ''} readOnly /></td>
+                </tr>
+                <tr>
+                    <td className={styles.label}>Address 1</td>
+                    <td><input type="text" value={customer.address1 || ''} readOnly /></td>
+                </tr>
+                <tr>
+                    <td className={styles.label}>Address 2</td>
+                    <td><input type="text" value={customer.address2 || ''} readOnly /></td>
+                </tr>
+                <tr>
+                    <td className={styles.label}>Town</td>
+                    <td><input type="text" value={customer.town || ''} readOnly /></td>
+                </tr>
+                <tr>
+                    <td className={styles.label}>PostCode</td>
+                    <td><input type="text" value={customer.postcode || ''} readOnly /></td>
+                </tr>
+                <tr>
+                    <td className={styles.label}>Country</td>
+                    <td><input type="text" value={customer.country || ''} readOnly /></td>
+                </tr>
+                <tr>
+                    <td className={styles.label}>Telephone</td>
+                    <td><input type="text" value={customer.telephone || ''} readOnly /></td>
+                </tr>
                 </tbody>
             </table>
         </div>
---
+++ src/example_data/customers.js
@@ -0,0 +1,68 @@
+const customers = [
+  {
+    customerNumber: "MOV001",
+    emailAddress: "brad.pitt@starlightfilms.com",
+    firstName: "Brad",
+    lastName: "Pitt",
+    business: "Pitt Productions",
+    address1: "120 Sunset Blvd",
+    address2: "Suite 5",
+    town: "Los Angeles",
+    postcode: "90028",
+    country: "United States",
+    telephone: "+1 213-555-0198"
+  },
+  {
+    customerNumber: "MOV002",
+    emailAddress: "aubrey.plaza@quirkycinema.net",
+    firstName: "Aubrey",
+    lastName: "Plaza",
+    business: "Quirky Cinema Co.",
+    address1: "44 Indie Lane",
+    address2: "",
+    town: "Portland",
+    postcode: "97209",
+    country: "United States",
+    telephone: "+1 503-555-0171"
+  },
+  {
+    customerNumber: "MOV003",
+    emailAddress: "keanu.reeves@matrixmail.com",
+    firstName: "Keanu",
+    lastName: "Reeves",
+    business: "Neo Solutions",
+    address1: "7 Cyber Drive",
+    address2: "Floor 3",
+    town: "San Francisco",
+    postcode: "94105",
+    country: "United States",
+    telephone: "+1 415-555-0144"
+  },
+  {
+    customerNumber: "MOV004",
+    emailAddress: "scarlett.johansson@blackwidow.org",
+    firstName: "Scarlett",
+    lastName: "Johansson",
+    business: "Red Room Films",
+    address1: "15 Marvel Plaza",
+    address2: "",
+    town: "New York",
+    postcode: "10001",
+    country: "United States",
+    telephone: "+1 212-555-0168"
+  },
+  {
+    customerNumber: "MOV005",
+    emailAddress: "ryan.reynolds@maximumeffort.tv",
+    firstName: "Ryan",
+    lastName: "Reynolds",
+    business: "Maximum Effort",
+    address1: "88 Chimichanga Way",
+    address2: "Penthouse 2",
+    town: "Vancouver",
+    postcode: "V6B 1L5",
+    country: "Canada",
+    telephone: "+1 604-555-0133"
+  }
+];
+export default customers;
